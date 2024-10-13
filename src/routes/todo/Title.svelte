83957b385d15058c419a80851e40d506acaaa56e
Kris Fragata <kristinne.fragata@associatedenvironmentalsystems.com>
2024-10-13 15:58:05 -0400

    feat/add activities

--- src/routes/todo/Title.svelte
+++ src/routes/todo/Title.svelte
@@ -1,13 +1,26 @@
 <script>
-	import SectionWrapper from "../../components/SectionWrapper.svelte";
- 
-
-
+	import SectionWrapper from '../../components/SectionWrapper.svelte';
 </script>
 
 <SectionWrapper id="title">
+	<div class="flex flex-col align-center justify-center">
+		<h1 class="text-center text-[50px] font-regular p-10">
+			<span class="lobster">There</span> is So much To Do
+		</h1>
 
-    <h1 class="text-center text-[50px] font-regular p-10">
-        <span class="lobster">There</span> is So much To Do
-    </h1>
+		<div class="text-left p-5">
+			<p class="p-1">+ driving to work = 2 hours</p>
+			<p class="p-1">+ scrolling on FB marketplace for furniture = 1 hour</p>
+			<p class="p-1">+ work = 8 hours</p>
+			<p class="p-1">+ cooking = 1.5 hours</p>
+			<p class="p-1">+ hanging out with cat = realistically 2 hours (expectedly, 25)</p>
+			<p class="p-1">+ music lessons = 2 hours/day (driving + lesson x 2)</p>
+			<p class="p-1">+ hanging out with partner = 3 hours</p>
+			<p class="p-1">+ climbing or running or anything to get my ass off a chair = 1-2 hours</p>
+			<p class="p-1">total: 20.5 hours</p>
+		</div>
+
+		<h2 class="text-center text-lg font-bold">Is this normal???</h2>
+		<p class="text-center text-sm">[it can't be]</p>
+	</div>
 </SectionWrapper>
