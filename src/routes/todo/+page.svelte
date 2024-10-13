cc119a11034c607e9a59df78b442b8616ee2956d
Kris Fragata <kristinne.fragata@associatedenvironmentalsystems.com>
2024-10-13 15:59:12 -0400

    fix arrow

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <main class="flex flex-col justify-center pl-50">
-        <a href="/" class="w-[50px] ">
+        <a href="/" class="w-[30px] pl-2">
         <img src={arrowLeft} alt="arrow-left"/>
     </a>
     <Title/>
